c7442a9c315ff7dd838c068eb461107ce832b547
A-007481D <labidabdelmalek@gmail.com>
2025-09-29 16:58:23 +0100

    feat(helper/utility): create some helper input methods

--- src/main/java/com/microfin/util/InputScanner.java
+++ src/main/java/com/microfin/util/InputScanner.java
@@ -21,4 +21,28 @@ public class InputScanner {
         return instance;
     }
 
+    public String readString(String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine().trim();
+    }
+
+    public int readInt(int prompt){
+        int value;
+        value = Integer.parseInt(scanner.nextLine().trim());
+        return value;
+    }
+
+    public double readDouble(double prompt){
+        double value;
+        value = Double.parseDouble(scanner.nextLine().trim());
+        return value;
+    }
+
+
+    public void close() {
+        if(scanner != null) {
+            scanner.close();
+        }
+    }
+
 }
